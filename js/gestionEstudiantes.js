a2abfeddf0f7bba5f29097acdb6d3e7b380e92fa
unknown <jdvargasr@uniboyaca.edu.co>
2023-06-04 10:39:25 -0500

    funcionalidad en estudiantes completa, borro archivo que use de guia

--- js/gestionEstudiantes.js
+++ js/gestionEstudiantes.js
@@ -21,9 +21,9 @@ $(document).ready(function () {
                     html += '      <td>';
                     html += '         <button class="eliminarBtn" data-id="' + estudiante.codigo + '">Eliminar</button>';
                     html += '      </td>';
-                    html+='        <td>';
-                    html+='          <button class="notasBtn" data-id="' + estudiante.codigo + '">Notas</button>';
-                    html+='        </td>';
+                    html += '        <td>';
+                    html += '          <button class="notasBtn" data-codigo="' + estudiante.codigo + '" data-nombres="' + estudiante.nombres + '" data-apellidos="' + estudiante.apellidos + '">Notas</button>';
+                    html += '        </td>';
                     html += '</tr>';
                 });
         
@@ -83,7 +83,6 @@ $(document).ready(function () {
             }
         }
 
-
         $(document).on("click", ".eliminarBtn", function() {
             const estCod = $(this).attr("data-id");
         
@@ -101,5 +100,71 @@ $(document).ready(function () {
             });
         });
         
-    
+
+        $(document).on("click", ".modificarBtn", function() {
+            const estCod = $(this).attr("data-id");
+        
+            // Encuentra la fila de la tabla más cercana al botón clickeado.
+            const fila = $(this).closest('tr');
+        
+            // Obtiene los valores de código, nombres y apellidos de las celdas correspondientes.
+            const codigo = fila.find('td:nth-child(1)').text();
+            const nombres = fila.find('td:nth-child(2)').text();
+            const apellidos = fila.find('td:nth-child(3)').text();
+        
+            // Crea elementos de input para los valores de código, nombres y apellidos.
+            const codigoInput = $('<input type="number" value="' + codigo + '">');
+            const nombresInput = $('<input type="text" value="' + nombres + '">');
+            const apellidosInput = $('<input type="text" value="' + apellidos + '">');
+        
+            // Reemplaza las celdas correspondientes con los inputs.
+            fila.find('td:nth-child(1)').html(codigoInput);
+            fila.find('td:nth-child(2)').html(nombresInput);
+            fila.find('td:nth-child(3)').html(apellidosInput);
+        
+            // Crea un botón "Guardar" para confirmar los cambios.
+            const guardarBtn = $('<button class="guardarCambiosBtn">Guardar</button>');
+            fila.find('td:last-child').html(guardarBtn);
+        
+            // Agrega un evento al botón "Guardar" para guardar los cambios.
+            guardarBtn.on('click', function() {
+                const nuevoCodigo = codigoInput.val();
+                const nuevosNombres = nombresInput.val();
+                const nuevosApellidos = apellidosInput.val();
+        
+                // Realiza la solicitud AJAX para modificar el estudiante.
+                $.ajax({
+                    url: 'http://localhost:8000/cambiarEstudiante/' + estCod,
+                    method: 'put',
+                    data: {
+                        codigo: nuevoCodigo,
+                        nombres: nuevosNombres,
+                        apellidos: nuevosApellidos
+                    }
+                }).done(function(response) {
+                    const dataJson = JSON.parse(response);
+                    const msg = dataJson.data;
+                    alert(msg);
+                    location.reload();
+                }).fail(function(error) {
+                    console.error(error);
+                });
+            });
+        });
+                
+                // Evento click en el botón Notas
+        $(document).on("click", ".notasBtn", function() {
+            const codigo = $(this).data("codigo");
+            const nombres = $(this).data("nombres");
+            const apellidos = $(this).data("apellidos");
+
+            // Almacenar la información en localStorage
+            localStorage.setItem("codigo", codigo);
+            localStorage.setItem("nombres", nombres);
+            localStorage.setItem("apellidos", apellidos);
+
+            // Redirigir a la página "actividades"
+            window.location.href = "actividades.html";
+        });
+
 
